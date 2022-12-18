e4c263c0ca7aa1251fca1af814172fab00f3ac05
julix14 <julix.beker@gmail.com>
2022-12-18 21:49:36 +0100

    [BIT-47] Fix Branch Setup

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <h1>THIS IS A DEMO SITE</h1>
+    <p> {{ message }} </p>
+    <p> {{ number }}</p>
+    <div>
+        <button @click="increment"> + </button>
+        <button @click="decrement"> - </button>
+    </div>
+
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+
+const message = "Hello Test!";
+
+const number = ref(0);
+
+const increment = () => {
+    number.value++;
+}
+const decrement = () => {
+    number.value--;
+}
+
+</script>
+
+<style scoped>
+
+</style>
