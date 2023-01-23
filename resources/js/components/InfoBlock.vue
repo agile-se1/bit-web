be54b475bbe0e01414a78b95d6418d3f9a7121a3
Juliusbecker <julix.becker@gmail.com>
2023-01-23 01:21:19 +0100

    [BIT-12] Add Menu Bar and InfoBlock

--- resources/js/components/InfoBlock.vue
+++ resources/js/components/InfoBlock.vue
@@ -1,16 +1,30 @@
 <script setup>
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    }
+});
 
 
 </script>
 
 
 <template>
-    <div class="w-full bg-blue-900">
-        <div class="container mx-auto">
-            <div class="flex flex-col items-center justify-center py-10">
-                <h1 class="text-4xl font-bold text-white">Informationen</h1>
-                <p class="text-xl text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
-            </div>
+    <a href="#"
+       class="flex flex-col p-6 mx-2 bg-white border border-gray-200 rounded-lg shadow min-h-600
+       hover:bg-gray-50">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-bit-blue">{{ title }}</h5>
+        <div class="text-gray-700 inline-block">
+            <p>{{ text }}</p>
         </div>
-    </div>
+    </a>
 </template>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,12 +1,29 @@
 <script setup>
 import MenuBar from "@/components/MenuBar.vue";
 import InfoBlock from "@/components/InfoBlock.vue";
+import {info} from "autoprefixer";
+
+const infoBlocks = {
+    basicInfos : {
+        title: "Was ist der Bit?",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+
+    }
+}
+
 </script>
 
 
-<template class="bg-bit-blue">
+<template>
+    <div class="top-0 sticky">
+        <MenuBar />
+    </div>
+    <div class="flex mt-52 justify-center">
+        <InfoBlock class="mx-6" :title="infoBlocks.basicInfos.title" :text="infoBlocks.basicInfos.text" url="#" />
+    </div>
 
-    <MenuBar />
+
+
 
 </template>
 
