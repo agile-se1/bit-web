dcb13380fa9ba40b53b7329a643d306d0176c5ca
Juliusbecker <julix.becker@gmail.com>
2023-01-23 01:48:20 +0100

    [BIT-12] Extract layout into own component

---
+++ resources/js/components/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="">
+        <div class="top-0 min-h-fit sticky-top">
+            <MenuBar />
+        </div>
+        <div class="min-h-screen mt-40">
+            <slot/>
+        </div>
+        <div class="">
+            <Footer />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import MenuBar from "@/components/MenuBar.vue";
+import Footer from "@/components/Footer.vue";
+</script>
+
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,7 +1,6 @@
 <script setup>
-import MenuBar from "@/components/MenuBar.vue";
 import InfoBlock from "@/components/InfoBlock.vue";
-import Footer from "@/components/Footer.vue";
+import Layout from "@/components/Layout.vue";
 
 const infoBlocks = {
     basicInfos : {
@@ -15,19 +14,11 @@ const infoBlocks = {
 
 
 <template>
-    <div class="">
-        <div class="top-0 min-h-fit sticky-top">
-            <MenuBar />
+    <layout>
+        <div class="flex justify-center min-h-fit">
+            <InfoBlock :title="infoBlocks.basicInfos.title" :text="infoBlocks.basicInfos.text" url="#" />
         </div>
-        <div class="min-h-screen mt-40">
-            <div class="flex justify-center min-h-fit">
-                <InfoBlock :title="infoBlocks.basicInfos.title" :text="infoBlocks.basicInfos.text" url="#" />
-            </div>
-        </div>
-        <div class="">
-            <Footer />
-        </div>
-    </div>
+    </layout>
 
 
 
