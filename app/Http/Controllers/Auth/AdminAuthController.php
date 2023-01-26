80ab2d4f149f6253c4269c7c58e6b01b501beedc
Chris <chrissy.drx@gmail.com>
2023-01-26 20:12:01 +0100

    [BIT-108] Fixed return types. Slight adjustments

--- app/Http/Controllers/Auth/AdminAuthController.php
+++ app/Http/Controllers/Auth/AdminAuthController.php
@@ -4,25 +4,30 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\Admin;
-use App\Models\User;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Contracts\View\View;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Routing\Redirector;
+use Illuminate\Http\RedirectResponse;
 use Session;
 
 
 class AdminAuthController extends Controller
 {
-
     public function __construct(){
         $this->middleware('guest')->except('logout');
         $this->middleware('guest:admin')->except('logout');
     }
 
-    public function showAdminLogin(){
+    public function showAdminLogin(): Factory|View|Application
+    {
         return view('admin.login');
     }
 
-    public function adminLogin(Request $request){
+    public function adminLogin(Request $request): Redirector|Application|RedirectResponse
+    {
         $this->validate($request, [
             'username' => ['required'],
             'password' => ['required', 'min:6'],
@@ -48,7 +53,8 @@ class AdminAuthController extends Controller
         return back()->withErrors('Could not lock in');
     }
 
-    public function logout (){
+    public function logout (): Redirector|Application|RedirectResponse
+    {
         Session::flush();
         Auth::logout();
 
