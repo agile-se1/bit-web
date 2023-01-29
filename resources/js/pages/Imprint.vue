7f043a4c7ee0896c6dc25c0991bb7e43d13663f8
Juliusbecker <julix.becker@gmail.com>
2023-01-29 16:02:11 +0100

    [BIT-12] Bug fixing

--- resources/js/pages/Imprint.vue
+++ resources/js/pages/Imprint.vue
@@ -1,48 +1,68 @@
 <template>
     <layout>
         <info-block title="Impressum" text="" class="mb-4">
-            <p>
-                <b>Freunde und Förderer des Gymnasiums Mellendorf e.V.</b><br/>
-                Fritz-Sennheiser-Platz 2<br/>
-                30900 Wedemark <br/>
-                <b>Telefon</b> +49 (0)5130/4542<br/>
-                <b>E-Mail:</b> info@ffgm.de<br/>
-            </p>
+            <template #below-text>
+                <p>
+                    <b>Freunde und Förderer des Gymnasiums Mellendorf e.V.</b><br/>
+                    Fritz-Sennheiser-Platz 2<br/>
+                    30900 Wedemark <br/>
+                    <b>Telefon</b> +49 (0)5130/4542<br/>
+                    <b>E-Mail:</b> info@ffgm.de<br/>
+                </p>
 
-            <p>
-                <b>Allein vertretungsberechtigt:</b> <br/><br/>
-                <b>1. Vorsitzende:</b> <br/>
-                Karen Drews <br/>
-                c/o  Fritz-Sennheiser-Platz 2 <br/>
-                30900 Wedemark  <br/>
-                <b>Telefon:</b> +49 (0)5130/4542 <br/>
-                <b>E-Mail:</b> karen.drews@ffgm.de <br/>
+                <p>
+                    <b>Allein vertretungsberechtigt:</b> <br/><br/>
+                    <b>1. Vorsitzende:</b> <br/>
+                    Karen Drews <br/>
+                    c/o  Fritz-Sennheiser-Platz 2 <br/>
+                    30900 Wedemark  <br/>
+                    <b>Telefon:</b> +49 (0)5130/4542 <br/>
+                    <b>E-Mail:</b> karen.drews@ffgm.de <br/>
 
-                <br/>
+                    <br/>
 
-                <b>Registergericht:</b> Amtsgericht Hannover <br/>
-                <b>Registernummer:</b> VR Nr. 120199 <br/>
-                <b>St.-Nr.:</b> 16/200/90349 <br/>
+                    <b>Registergericht:</b> Amtsgericht Hannover <br/>
+                    <b>Registernummer:</b> VR Nr. 120199 <br/>
+                    <b>St.-Nr.:</b> 16/200/90349 <br/>
 
-                <br/>
+                    <br/>
 
-                Die Freunde und Förderer des Gymnasiums Mellendorf e.V. sind  im Sinne des § 52 Abs. 2 Satz 1 Nr. 7 AO als gemeinnützig anerkannt.
-            </p>
+                    Die Freunde und Förderer des Gymnasiums Mellendorf e.V. sind  im Sinne des § 52 Abs. 2 Satz 1 Nr. 7 AO als gemeinnützig anerkannt.
+                </p>
 
-            <p>
-                <b>1. Haftungsbeschränkung</b> <br/>
-                Die Inhalte dieser Website werden mit größtmöglicher Sorgfalt erstellt. Der Anbieter übernimmt jedoch keine Gewähr für die Richtigkeit, Vollständigkeit und Aktualität der bereitgestellten Inhalte. Die Nutzung der Inhalte der Website erfolgt auf eigene Gefahr des Nutzers. <br/>
-                Namentlich gekennzeichnete Beiträge geben die Meinung des jeweiligen Autors und nicht immer die Meinung des Anbieters wieder. Mit der reinen Nutzung der Website des Anbieters kommt keinerlei Vertragsverhältnis zwischen dem Nutzer und dem Anbieter zustande. <br/>
-                <br/>
-                <b>2. Externe Links</b> <br/>
-                Diese Website enthält Verknüpfungen zu Websites Dritter ("externe Links"). Diese Websites unterliegen der Haftung der jeweiligen Betreiber. Der Anbieter hat bei der erstmaligen Verknüpfung der externen Links die fremden Inhalte daraufhin überprüft, ob etwaige Rechtsverstöße bestehen. Zu dem Zeitpunkt waren keine Rechtsverstöße ersichtlich. Der Anbieter hat keinerlei Einfluss auf die aktuelle und zukünftige Gestaltung und auf die Inhalte der verknüpften Seiten. Das Setzen von externen Links bedeutet nicht, dass sich der Anbieter die hinter dem Verweis oder Link liegenden Inhalte zu Eigen macht. Eine ständige Kontrolle der externen Links ist für den Anbieter ohne konkrete Hinweise auf Rechtsverstöße nicht zumutbar. Bei Kenntnis von Rechtsverstößen werden jedoch derartige externe Links unverzüglich gelöscht. <br/>
-                <br/>
-                <b>3. Urheber- und Leistungsschutzrechte</b> <br/>
-                Die auf dieser Website veröffentlichten Inhalte unterliegen dem deutschen Urheber- und Leistungsschutzrecht. Jede vom deutschen Urheber- und Leistungsschutzrecht nicht zugelassene Verwertung bedarf der vorherigen schriftlichen Zustimmung des Anbieters oder jeweiligen Rechteinhabers. Dies gilt insbesondere für Vervielfältigung, Bearbeitung, Übersetzung, Einspeicherung, Verarbeitung bzw. Wiedergabe von Inhalten in Datenbanken oder anderen elektronischen Medien und Systemen. Inhalte und Rechte Dritter sind dabei als solche gekennzeichnet. Die unerlaubte Vervielfältigung oder Weitergabe einzelner Inhalte oder kompletter Seiten ist nicht gestattet und strafbar. Lediglich die Herstellung von Kopien und Downloads für den persönlichen, privaten und nicht kommerziellen Gebrauch ist erlaubt. Die Darstellung dieser Website in fremden Frames ist nur mit schriftlicher Erlaubnis zulässig. <br/>
-            </p>
-
-
-
+                <p>
+                    <b>1. Haftungsbeschränkung</b> <br/>
+                    Die Inhalte dieser Website werden mit größtmöglicher Sorgfalt erstellt. Der Anbieter übernimmt
+                    jedoch keine Gewähr für die Richtigkeit, Vollständigkeit und Aktualität der bereitgestellten
+                    Inhalte. Die Nutzung der Inhalte der Website erfolgt auf eigene Gefahr des Nutzers. <br/>
+                    Namentlich gekennzeichnete Beiträge geben die Meinung des jeweiligen Autors und nicht immer die
+                    Meinung des Anbieters wieder. Mit der reinen Nutzung der Website des Anbieters kommt keinerlei
+                    Vertragsverhältnis zwischen dem Nutzer und dem Anbieter zustande. <br/>
+                    <br/>
+                    <b>2. Externe Links</b> <br/>
+                    Diese Website enthält Verknüpfungen zu Websites Dritter ("externe Links"). Diese Websites
+                    unterliegen der Haftung der jeweiligen Betreiber. Der Anbieter hat bei der erstmaligen Verknüpfung
+                    der externen Links die fremden Inhalte daraufhin überprüft, ob etwaige Rechtsverstöße bestehen. Zu
+                    dem Zeitpunkt waren keine Rechtsverstöße ersichtlich. Der Anbieter hat keinerlei Einfluss auf die
+                    aktuelle und zukünftige Gestaltung und auf die Inhalte der verknüpften Seiten. Das Setzen von
+                    externen Links bedeutet nicht, dass sich der Anbieter die hinter dem Verweis oder Link liegenden
+                    Inhalte zu Eigen macht. Eine ständige Kontrolle der externen Links ist für den Anbieter ohne
+                    konkrete Hinweise auf Rechtsverstöße nicht zumutbar. Bei Kenntnis von Rechtsverstößen werden jedoch
+                    derartige externe Links unverzüglich gelöscht. <br/>
+                    <br/>
+                    <b>3. Urheber- und Leistungsschutzrechte</b> <br/>
+                    Die auf dieser Website veröffentlichten Inhalte unterliegen dem deutschen Urheber- und
+                    Leistungsschutzrecht. Jede vom deutschen Urheber- und Leistungsschutzrecht nicht zugelassene
+                    Verwertung bedarf der vorherigen schriftlichen Zustimmung des Anbieters oder jeweiligen
+                    Rechteinhabers. Dies gilt insbesondere für Vervielfältigung, Bearbeitung, Übersetzung,
+                    Einspeicherung, Verarbeitung bzw. Wiedergabe von Inhalten in Datenbanken oder anderen elektronischen
+                    Medien und Systemen. Inhalte und Rechte Dritter sind dabei als solche gekennzeichnet. Die unerlaubte
+                    Vervielfältigung oder Weitergabe einzelner Inhalte oder kompletter Seiten ist nicht gestattet und
+                    strafbar. Lediglich die Herstellung von Kopien und Downloads für den persönlichen, privaten und
+                    nicht kommerziellen Gebrauch ist erlaubt. Die Darstellung dieser Website in fremden Frames ist nur
+                    mit schriftlicher Erlaubnis zulässig. <br/>
+                </p>
+            </template>
         </info-block>
 
     </layout>
@@ -52,7 +72,5 @@
 <script setup>
 
 
-
 import Layout from "@/components/Layout.vue";
-import InfoBlock from "@/components/InfoBlock.vue";
-</script>
+import InfoBlock from "@/components/InfoBlock.vue";</script>
