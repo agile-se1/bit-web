13210001014a3cb9e7eab5767057508903bb892f
Juliusbecker <julix.becker@gmail.com>
2023-02-02 17:58:37 +0100

    [BIT-21] Change text in footer

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -2,16 +2,13 @@
     <footer class="w-full bg-bit-blue flex justify-between min-h-full px-6">
         <Link href="/impressum" class="text-white py-6">Impressum</Link>
 
-        <p class="text-white py-6">Copyright C.Matthias & his mommy {{ year }}</p>
-
+        <div class="text-white py-6">
+            &copy; {{ year }} - Freunde & Förderer Gymnasium Mellendorf
+        </div>
         <Link href="/privacy" class="text-white py-6">Datenschutz</Link>
     </footer>
 </template>
 
 <script setup>
 const year = new Date().getFullYear();
-import { Link } from '@inertiajs/inertia-vue3'
-
-
-
-</script>
+import {Link} from '@inertiajs/inertia-vue3'</script>
