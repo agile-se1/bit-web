945edf0484741a07a6579d8e60e842f75d0260f5
Chris <chrissy.drx@gmail.com>
2023-02-23 12:09:30 +0100

    [Bit-120] Login Email finished

--- resources/views/emails/loginLinkMail-text.blade.php
+++ resources/views/emails/loginLinkMail-text.blade.php
@@ -1,2 +1,14 @@
-Email with plain text
-Link: {{ $link }}
+Hallo {{ $first_name }},
+Die Berufsfeldwahl ist nun freigeschaltet. Bitte öffnenen Sie diesen Link ( {{ $link }} ), um eine Auswahl zu treffen.
+
+Um Berufsfelder und Vortrag auszuwählen, folgen Sie bitte diesen Schritten:
+1. Klicken Sie auf den Knopf, der Sie zur Bereufswahlseite weiterleitet
+2. Wählen Sie einen allgemeinen Vortrag und zwei Berufsfelder aus
+3. Bestätigen Sie ihre Wahl
+
+Sollten Sie noch Fragen haben, können Sie sich an info@gm-bit.de eine Email schicken.
+
+Viel Erfolg bei der Auswahl
+
+Mit freundlichen Grüßen
+Alexandra Matthaei
