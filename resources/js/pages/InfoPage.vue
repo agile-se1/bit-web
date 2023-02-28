7edbad0c6ad59d72df098a9092d591232f5d7e79
Juliusbecker <julix.becker@gmail.com>
2023-02-28 19:11:45 +0100

    [BIT-19] Add Content

--- resources/js/pages/InfoPage.vue
+++ resources/js/pages/InfoPage.vue
@@ -1,7 +1,149 @@
 <template>
+    <Layout>
+        <div class="flex flex-col">
+            <p class="text-bit-blue text-6xl text-center font-bold">Der BIT am Gymnasium Mellendorf</p>
+
+            <div class="bg-bit-blue w-full flex flex-col mt-5">
+                <div class="p-4 mx-32">
+                    <p class="text-white text-center font-semibold text-3xl">Wie alles begann:</p>
+                    <p class="text-center text-white">
+                        Aufgrund des großen Wunsches zahlreicher Schüler und Eltern, dem Thema Berufsinformation am
+                        Gymnasium Mellendorf einen höheren Stellenwert einzuräumen, organisierten Karen Drews (Erste
+                        Vorsitzende des FFGM) und Jutta Holtmann (damals SER-Vorstand) im Jahr 2008 den ersten
+                        <strong>B</strong>erufs<strong>I</strong>nformations<strong>T</strong>ag des Gymnasium
+                        Mellendorfs.
+                    </p>
+                </div>
+            </div>
+
+            <div class="flex justify-around mt-5 mx-32">
+                <div class="flex flex-col bg-blue-200 p-2 rounded">
+                    <img src="../../assets/img_3.png" alt="Foto von Alexandra Matthaei"
+                         class="max-h-60 rounded border-8 border-bit-blue shadow-lg">
+                    <label class="text-center text-bit-blue">
+                        Alexandra Matthaei <br>
+                        Aktuelle Organisatorin des BIT
+                    </label>
+                </div>
+                <div class="flex flex-col bg-blue-200 p-2 rounded">
+                    <img src="../../assets/img_4.png" alt="Foto von Karen Drews"
+                         class="max-h-60 rounded border-8 border-bit-blue shadow-lg">
+                    <label class="text-center text-bit-blue">
+                        Karen Drews <br>
+                        Gründerin des BIT
+                    </label>
+                </div>
+                <div class="flex flex-col bg-blue-200 p-2 rounded">
+                    <img src="../../assets/img_5.png" alt="Foto von Jutta Holtmann"
+                         class="max-h-60 rounded border-8 border-bit-blue shadow-lg">
+                    <label class="text-center text-bit-blue">
+                        Jutta Holtmann <br>
+                        Gründerin des BIT
+                    </label>
+                </div>
+            </div>
+
+            <div class="w-full flex flex-col mt-10">
+                <div class="p-4 mx-32">
+                    <p class="text-bit-blue text-center font-semibold text-xl">Als Referenten für diese Veranstaltung
+                        wurden Eltern, Fördervereinsmitglieder und vor allem ehemalige des Gymnasiums Mellendorf
+                        angesprochen. Dies motivierte die beiden engagierten Gründer*innen zur weiteren Planung der
+                        Veranstaltung, deren Ziele damals wie heute in erster Linie sein sollen:
+
+                    </p>
+                    <ul class="list-disc text-bit-blue list-inside">
+                        <li>
+                            Zusammenführung unseres 12. Jahrgangs mit Eltern, Ehemaligen, Firmen aus der Wedemark oder
+                            der Region, die in unterschiedlichen Berufen arbeiten
+                        </li>
+                        <li>
+                            Vermittlung von Informationen über verschiedene Berufsfelder und Studienmöglichkeiten
+                        </li>
+                        <li>
+                            Einblick in persönliche Werdegänge, insbesondere solche, die trotz Brüchen erfolgreich sind,
+                            so dass den jungen Menschen die Angst vor etwaigen Fehlentscheidungen genommen wird.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class=" flex flex-col mt-10">
+                <div class="p-4 mx-32 flex flex-col items-center">
+                    <p class="text-bit-blue text-center font-semibold text-2xl mb-2">Übersicht der Berufsfelder: </p>
+                    <div class="max-w-fit">
+                        <ul class="list-disc text-bit-blue columns-2 list-inside">
+                            <li class="bg-blue-200 rounded-full px-2 max-w-fit my-1">
+                                Polizei / Zoll
+                            </li>
+                            <li class="bg-blue-200 rounded-full px-2 max-w-fit my-1">
+                                Bundeswehr / zivile Luftfahrt
+                            </li>
+                            <li class="bg-blue-200 rounded-full px-2 max-w-fit my-1">
+                                Gestaltung / Kunst / Design
+                            </li>
+                            <li class="bg-blue-200 rounded-full px-2 max-w-fit my-1">
+                                Medizin / Pflege / Therapie
+                            </li>
+                            <li class="bg-blue-200 rounded-full px-2 max-w-fit my-1">
+                                Medien / Mediengestaltung / Politik
+                            </li>
+                            <li class="bg-blue-200 rounded-full px-2 max-w-fit my-1">
+                                Architektur
+                            </li>
+                            <li class="bg-blue-200 rounded-full px-2 max-w-fit my-1">
+                                Sport / Freizeit
+                            </li>
+                            <li class="bg-blue-200 rounded-full px-2 max-w-fit my-1">
+                                Eventmanagement / Tourismus
+                            </li>
+                            <li class="bg-blue-200 rounded-full px-2 max-w-fit my-1">
+                                Jura / Steuerberatung / Logistik
+                            </li>
+                            <li class="bg-blue-200 rounded-full px-2 max-w-fit my-1">
+                                Naturwissenschaften / Technik
+                            </li>
+                            <li class="bg-blue-200 rounded-full px-2 max-w-fit my-1">
+                                Soziales / Pädagogik / Erziehung
+                            </li>
+                            <li class="bg-blue-200 rounded-full px-2 max-w-fit my-1">
+                                Maschinenbau / Elektrotechnik / Informatik
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col text-bit-blue my-5">
+                <p class="text-bit-blue text-center font-semibold text-3xl">Ablauf</p>
+                <div class="text-bit-blue rounded mx-32 py-2">
+                    <p> Nach einer Begrüßung und dem „allgemeinen Teil“, in dem die Schüler*innen zeitgleich je nach
+                        Interesse Informationen zum Universitätsstudium, zum dualen Studium (Industrie- und
+                        Handelskammer) sowie zu Ausbildungsmöglichkeiten im Handwerk (Handwerkskammer) erhalten, findet
+                        eine Kaffeepause statt.
+                    </p>
+                </div>
+                <div class="text-bit-blue rounded mx-32 py-2">
+                    <p> Anschließend versammeln sich die Schülergruppen je nach Wahl in den entsprechenden
+                        Berufsfeldgruppen und nach ca. einer Stunde wechseln die Schüler*innen ihres zweiten Wunsches.
+                        Den Schüler*innen stehen hervorragende Referenten Rede und Antwort; vielfach sind es junge
+                        Menschen am Anfang der Berufstätigkeit bzw. in Ausbildung, deren Ausführungen von langjährige
+                        Berufstätigen mit großer Erfahrung ergänzt werden – oder auch umgekehrt, kurz Informationen aus
+                        vielerlei Perspektiven werden weitergegeben.
+                    </p>
+                </div>
+                <div class="text-bit-blue rounded mx-32 py-2">
+                    <p>
+                        Ein Mittagsimbiss mit vielen netten und anregenden Gesprächen zwischen den Referenten,
+                        Schüler*innen, Eltern und Lehrern bildet den Abschluss des Vormittags.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </Layout>
 
 </template>
 
 <script setup>
 
+import Layout from "@/components/Layout.vue";
 </script>
