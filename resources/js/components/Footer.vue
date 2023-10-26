c2d2f8e12b3ce578d72a8c54b5d25f0e49c7ceef
Juliusbecker <julix.becker@gmail.com>
2023-10-26 12:03:14 +0200

    [BIT-FinalMakeOver] Make footer responsive

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,13 +1,34 @@
 <template>
-    <footer class="w-full bg-bit-blue flex justify-between min-h-full px-6">
-        <Link href="/impressum" class="text-white py-6">Impressum</Link>
+    <footer class="bg-white drop-shadow-2xl md:py-2">
+        <div class="w-full max-w-screen-xl mx-auto p-2 ">
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <div class="flex shrink">
+                    <a href="https://www.ffgm.schule/index.php/start.html" target="_blank" class="flex items-center mb-4 sm:mb-0">
+                        <img src="/logo.png" class="h-8 mr-3" alt="Förderverein Logo" />
+                    </a>
+                </div>
+                <ul class="flex flex-wrap items-center justify-between mb-6 text-sm font-medium text-bit-blue sm:mb-0">
+                    <li class="mr-4 hover:underline md:mr-6">
+                        <Link href="/impressum">Impressum</Link>
+                    </li>
 
-        <div class="text-white py-6">
-            &copy; {{ year }} - Freunde & Förderer Gymnasium Mellendorf
+                    <li class="mr-4 hover:underline md:mr-6">
+                        <Link href="/privacy">Datenschutz</Link>
+                    </li>
+
+                    <li class="hover:underline">
+                        <Link href="/arrival">Anfahrt</Link>
+                    </li>
+                </ul>
+            </div>
+            <hr class="my-2 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-2" />
+            <span class="block text-sm text-bit-blue text-center">  &copy; {{ year }} - Freunde & Förderer Gymnasium Mellendorf</span>
         </div>
-        <Link href="/privacy" class="text-white py-6">Datenschutz</Link>
     </footer>
 </template>
+
+
+
 
 <script setup>
 const year = new Date().getFullYear();
