e2d723fe165693ca68edd75270e4377d440b4b83
Juliusbecker <julix.becker@gmail.com>
2023-10-27 17:03:59 +0200

    [BIT-FinalMakeOver] Change Image

--- resources/js/components/InfoBlock.vue
+++ resources/js/components/InfoBlock.vue
@@ -16,12 +16,11 @@ defineProps({
 
 <template>
     <div class="flex flex-col md:flex-row p-6 mx-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-50">
-        <div class="md:w-1/2">
+        <div class="md:w-1/2 md:flex-col md:flex md:justify-center">
             <p class="mb-2 text-2xl font-bold tracking-tight text-bit-blue">{{ title }}</p>
             <div class="text-gray-700">
                 <p>{{ text }}</p>
             </div>
-            <slot name="below-text" />
         </div>
         <div class="md:w-1/2 mt-4 md:mt-0">
             <slot name="images" />
