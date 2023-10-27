f6e5060d1ad32566ac16760c71e3bdfb1bcc971d
Juliusbecker <julix.becker@gmail.com>
2023-10-27 18:19:53 +0200

    [BIT-FinalMakeOver] Make ReferentenInfo Responsive

--- resources/js/pages/Arrival.vue
+++ resources/js/pages/Arrival.vue
@@ -1,8 +1,11 @@
 <template>
     <Layout>
         <div class="flex flex-col items-center min-w-full min-h-full">
-        <p class="text-bit-blue text-3xl">Anfahrt</p>
-        <iframe class="google-map p-4 m-5 min-h-1/2 min-w-1/2 flex-grow" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2432.958370969627!2d9.615284216506913!3d52.42555197979622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b07113df15f1ed%3A0xac56b13dd80b3fd7!2sTEWISS%20-%20Technik%20und%20Wissen%20GmbH!5e0!3m2!1sde!2sde!4v1571311822456!5m2!1sde!2sde"></iframe>
+
+
+
+            <p class="text-bit-blue text-3xl">Anfahrt</p>
+            <iframe class="google-map p-4 m-5 min-h-1/2 min-w-1/2 flex-grow" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2432.958370969627!2d9.615284216506913!3d52.42555197979622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b07113df15f1ed%3A0xac56b13dd80b3fd7!2sTEWISS%20-%20Technik%20und%20Wissen%20GmbH!5e0!3m2!1sde!2sde!4v1571311822456!5m2!1sde!2sde"></iframe>
         </div>
     </Layout>
 </template>
