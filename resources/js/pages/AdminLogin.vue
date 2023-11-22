7c6f69d1929844f7f49fe60e6b6c5f9ea5a22289
Juliusbecker <julix.becker@gmail.com>
2023-11-22 14:40:47 +0100

    [BIT] Fix: Form

--- resources/js/pages/AdminLogin.vue
+++ resources/js/pages/AdminLogin.vue
@@ -1,14 +1,13 @@
 <template>
     <Layout type="admin">
         <div class="flex flex-col justify-around">
-            <form class="grid border-gray-200 border border-solid rounded p-5 w-72">
+            <form class="grid border-gray-200 border border-solid rounded p-5 w-72" @submit.prevent="submit">
                 <p class="text-2xl text-bit-blue">Admin Login</p>
                 <p v-if="errors.username" class="text-red-700 text-xs">Fehler: {{ errors.username }}</p>
                 <p v-if="errors.password" class="text-red-700 text-xs">Fehler: {{ errors.password }}</p>
                 <input class="p-2 my-2 rounded" v-model="username" type="text" placeholder="Benutzername">
                 <input class="p-2 my-2 rounded" v-model="password" type="password" placeholder="Passwort">
-                <button class="bg-bit-blue text-white rounded p-2 my-2" type="submit"
-                        @click="login">Login
+                <button class="bg-bit-blue text-white rounded p-2 my-2" type="submit">Login
                 </button>
             </form>
         </div>
@@ -32,7 +31,7 @@ defineProps({
     }
 )
 
-function login() {
+function submit() {
     Inertia.post('/admin/login', {
         username: username.value,
         password: password.value
